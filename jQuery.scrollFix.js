612723b5c131a695baaa77756650cd7170dfc2dd
mengqing <346793149@qq.com>
2016-10-26 18:36:59 +0800

    add start fixed

--- jQuery.scrollFix.js
+++ jQuery.scrollFix.js
@@ -15,23 +15,30 @@
 
     $.fn.scrollFix = function(options) {
         var defaults = {
-            top: 0,         // 固定在顶部的高度
-            bottom: 0,      // 停靠在底部的位置
-            endObj: ''      // 滑到这个位置顶部时开始跟随滚动
+            top: 0,             // 固定在顶部的高度
+            bottom: 0,          // 停靠在底部的位置
+            zindex: 999,        // 位置索引
+            startObj: '',       // 滑到 startObj 位置时开始浮动固定，默认为空(即 this)
+            position: 'top',    // 滑到 startObj 顶端/底端 开始浮动固定 ('top'|'bottom')
+            endObj: ''          // 滑到这个位置顶部时取消固定并继续跟随滚动
         }
         var opts = $.extend({}, defaults, options);
-        var $this = $(this),
-            isEnd = (opts.endObj !== '' || opts.endObj !== undefined) ? true : false;
-        var eleTop = $this.offset().top - opts.top,
-            endTop = isEnd ? ($(opts.endObj).offset().top - opts.bottom - opts.top) : 0;
+        var $this = $(this),        // 当前对象
+            isStartObj = (opts.startObj === '' || opts.startObj === undefined) ? false : true,      // 是否从当前对象开始浮动
+            isEndObj = (opts.endObj === '' || opts.endObj === undefined) ? false : true;            // 是否取消浮动固定
+        var eleHeight = isStartObj ? (opts.position === 'top' ? 0 : $(opts.startObj).outerHeight()) : 0;       // (顶部|底部) 开始浮动
+            eleTop = $this.offset().top - opts.top,
+            startTop = isStartObj ? ($(opts.startObj).offset().top + eleHeight - opts.top) : eleTop,
+            endTop = isEndObj ? ($(opts.endObj).offset().top - opts.bottom - opts.top) : 0;
         var maxY = endTop - $(this).outerHeight();
+
 
         var methods = {
             fixed: function() {
-                $this.css({position: 'fixed', top: opts.top + 'px'});
+                $this.css({position: 'fixed', top: opts.top + 'px', zIndex: opts.zindex});
             },
             absolute: function() {
-                $this.css({position: 'absolute', top: (maxY - eleTop) + 'px'});
+                $this.css({position: 'absolute', top: (maxY - eleTop) + 'px', zIndex: opts.zindex});
             },
             none: function() {
                 $this.removeAttr('style');
@@ -40,8 +47,8 @@
 
         $(window).scroll(function(event) {
             var scrollTop = $(this).scrollTop();
-            if(scrollTop > eleTop) {
-                if(isEnd) {
+            if(scrollTop > startTop) {
+                if(isEndObj) {
                     if(maxY < scrollTop) {
                         methods.absolute();
                     } else {
