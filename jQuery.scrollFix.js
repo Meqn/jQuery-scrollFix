fdd100a8ea56f476bcf25820c1453e15fe232dda
mengqing <346793149@qq.com>
2016-11-01 18:26:05 +0800

    update gulpfile

--- jQuery.scrollFix.js
+++ jQuery.scrollFix.js
@@ -2,11 +2,12 @@
  * jQuery.scrollFix 滚动固定在某个位置的jQuery插件
  * by: mengqing723@gmail.com
  * date: 2016/10/26
-  */
+*/
+
 ;(function (factory) {
     if (typeof define === 'function' && define.amd) {
         define(['jquery'], factory);
-    } else if (typeof exports === 'object' && module.exports) {
+    } else if (typeof exports === 'object' && exports) {
         module.exports = factory;
     } else {
         factory(jQuery);
@@ -18,20 +19,19 @@
             top: 0,             // 固定在顶部的高度
             bottom: 0,          // 停靠在底部的位置
             zindex: 999,        // 位置索引
-            startObj: '',       // 滑到 startObj 位置时开始浮动固定，默认为空(即 this)
+            startObj: null,     // 滑到 startObj 位置时开始浮动固定，默认为空(即 当前元素)
             position: 'top',    // 滑到 startObj 顶端/底端 开始浮动固定 ('top'|'bottom')
-            endObj: ''          // 滑到这个位置顶部时取消固定并继续跟随滚动
+            endObj: null        // 滑到这个位置顶部时取消固定并继续跟随滚动
         }
         var opts = $.extend({}, defaults, options);
         var $this = $(this),        // 当前对象
-            isStartObj = (opts.startObj === '' || opts.startObj === undefined) ? false : true,      // 是否从当前对象开始浮动
-            isEndObj = (opts.endObj === '' || opts.endObj === undefined) ? false : true;            // 是否取消浮动固定
+            isStartObj = opts.startObj === null ? false : true,         // 是否从当前对象开始浮动
+            isEndObj = opts.endObj === null ? false : true;             // 是否结束浮动固定
         var eleHeight = isStartObj ? (opts.position === 'top' ? 0 : $(opts.startObj).outerHeight()) : 0;       // (顶部|底部) 开始浮动
             eleTop = $this.offset().top - opts.top,
             startTop = isStartObj ? ($(opts.startObj).offset().top + eleHeight - opts.top) : eleTop,
             endTop = isEndObj ? ($(opts.endObj).offset().top - opts.bottom - opts.top) : 0;
         var maxY = endTop - $(this).outerHeight();
-
 
         var methods = {
             fixed: function() {
@@ -61,6 +61,5 @@
                 methods.none();
             }
         });
-        
     }
 }));
